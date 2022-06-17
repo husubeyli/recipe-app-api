1572749f7d2b4c0a98333fa1190af575873d0830
husubeyli <husubeyli@gmail.com>
2022-06-17 10:21:53 +0400

    first commit

--- .venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/husubayli/Desktop/Documents/study/recipe-app-api/.venv/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
